f54471380981fb8a9dc75d4d8c305767566b3389
ebene88 <abenezernachew50@gmail.com>
2022-09-29 18:59:54 +0300

    initial hangman display

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import pygame
 import os
 
+# setup display
 pygame.init()
 WIDTH, HEIGHT = 800, 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman Game!")
 
+# load images
 
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman"+str(i)+".png")
+    images.append(image)
 
+print(images)
 
+# game variables
+hangman_status = 0
+
+# colors
+WHITE = (255,255,255)
+
+# setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
 while run:
     clock.tick(FPS)
+
+    win.fill(WHITE)
+    win.blit(images[hangman_status],(150, 140))
+    pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
